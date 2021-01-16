e0a3431a67bd3f89fc564708ace898a502e83dfd
Andra Satria Pratama <andrasatriapratama@gmail.com>
2021-01-16 18:55:40 +0700

    basic project

---
+++ my_first_dart_console/bin/main.dart
@@ -0,0 +1,9 @@
+
+import 'dart:io';
+
+void main(List<String> arguments) {
+  String input = stdin.readLineSync();
+  double input2 = double.tryParse(stdin.readLineSync()) ;
+  print(input);
+  print(input2);
+}
